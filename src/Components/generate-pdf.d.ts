f3a0b8be76462e715763f992877fd053171767f5
Amine <amine.benmoussa1994@gmail.com>
2025-02-03 09:58:52 +0100

    Prepare for Vercel deployment

---
+++ src/Components/generate-pdf.d.ts
@@ -0,0 +1,3 @@
+declare const GeneratePdf: () => import("react/jsx-runtime").JSX.Element;
+export default GeneratePdf;
+//# sourceMappingURL=generate-pdf.d.ts.map
---
+++ src/Components/login.d.ts
@@ -0,0 +1,7 @@
+export declare const months: {
+    name: string;
+    value: string;
+}[];
+declare const Login: () => import("react/jsx-runtime").JSX.Element;
+export default Login;
+//# sourceMappingURL=login.d.ts.map
